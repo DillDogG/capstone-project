b3bcf91dd4b504b8f797c3247d180ac93770c8e9
DillDogG <dilldoggaming@gmail.com>
2024-10-08 16:36:35 -0600

    Finishing player movement

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,24 +4,31 @@ using System;
 public partial class Player : CharacterBody3D
 {
 	[Export]
-	public int Speed { get; set; } = 14;
+	public int Speed { get; set; } = 20;
 
     // Vertical impulse applied to the character upon jumping in meters per second.
     [Export]
-    public int JumpImpulse { get; set; } = 20;
+    public int JumpImpulse { get; set; } = 30;
 
     // The downward acceleration when in the air, in meters per second squared.
     [Export]
     public int FallAcceleration { get; set; } = 75;
 
+    public bool Sprinting { get; set; } = false;
+    public bool Crouching { get; set; } = false;
+
+    private int JumpCount { get; set; }
+
     private Vector3 _targetVelocity = Vector3.Zero;
 
+    Node3D pivot;
     //private Vector2 prevMousePos = Vector2.Zero;
 
     public override void _Ready()
     {
         base._Ready();
         Input.MouseMode = Input.MouseModeEnum.Captured;
+        pivot = GetNode<Node3D>("Pivot");
     }
 
     public override void _PhysicsProcess(double delta)
@@ -29,6 +36,11 @@ public partial class Player : CharacterBody3D
         // We create a local variable to store the input direction.
         var direction = Vector3.Zero;
 
+        // resetting extra jumps while on the ground
+        if (IsOnFloor())
+        {
+            JumpCount = 2;
+        }
         // We check for each move input and update the direction accordingly.
         // original is commented out, swap if it works again
         if (Input.IsActionPressed("move_right"))
@@ -57,13 +69,31 @@ public partial class Player : CharacterBody3D
         if (direction != Vector3.Zero)
         {
             direction = direction.Normalized();
-            // Setting the basis property will affect the rotation of the node.
-            //GetNode<Node3D>("Pivot").Basis = Basis.LookingAt(direction);
+            direction = direction.Rotated(new Vector3(0, 1, 0), pivot.Rotation.Y);
+            direction = direction.Normalized();
         }
+
+        if (Input.IsActionPressed("sprint")) Sprinting = true;
+        else Sprinting = false;
+
+        if (Input.IsActionPressed("crouch")) Crouching = true;
+        else Crouching = false;
 
         // Ground velocity
         _targetVelocity.X = direction.X * Speed;
         _targetVelocity.Z = direction.Z * Speed;
+        if (Sprinting && !Crouching)
+        {
+            _targetVelocity.X *= 2;
+            _targetVelocity.Z *= 2;
+        }
+        
+        // gives a burst of movement when ending a slide
+        if (Input.IsActionJustPressed("crouch"))
+        {
+            _targetVelocity.X *= 3;
+            _targetVelocity.Z *= 3;
+        }
 
         // Vertical velocity
         if (!IsOnFloor()) // If in the air, fall towards the floor. Literally gravity
@@ -76,9 +106,29 @@ public partial class Player : CharacterBody3D
         {
             _targetVelocity.Y = JumpImpulse;
         }
+        else if (JumpCount > 0 && Input.IsActionJustPressed("jump"))
+        {
+            _targetVelocity.Y = JumpImpulse;
+            JumpCount--;
+        }
 
-        // Moving the character
-        Velocity = _targetVelocity;
+        // when releasing jump, slow down significantly to give more control when jumping
+        if (Input.IsActionJustReleased("jump") && Velocity.Y > 0)
+        {
+            _targetVelocity.Y *= 0.25f;
+        }
+
+        // Moving the character if sliding / else not sliding
+        if (Crouching && IsOnFloor())
+        {
+            _targetVelocity.X = (Math.Abs(Velocity.X) > Math.Abs(_targetVelocity.X) * 0.5f) ? Velocity.X * 0.99f : _targetVelocity.X * 0.5f;
+            _targetVelocity.Z = (Math.Abs(Velocity.Z) > Math.Abs(_targetVelocity.Z) * 0.5f) ? Velocity.Z * 0.99f : _targetVelocity.Z * 0.5f;
+            // enable to slow down sliding at a point
+            _targetVelocity.X = (Math.Abs(_targetVelocity.X) < 2) ? 0 : _targetVelocity.X;
+            _targetVelocity.Z = (Math.Abs(_targetVelocity.Z) < 2) ? 0 : _targetVelocity.Z;
+            Velocity = _targetVelocity;
+        }
+        else Velocity = _targetVelocity;
         MoveAndSlide();
     }
 
@@ -87,16 +137,7 @@ public partial class Player : CharacterBody3D
         if (@event is InputEventMouseMotion motionEvent)
         {
             Vector2 mouseMovement = motionEvent.ScreenRelative;
-            //mouseMovement.Y = clamp(mouseMovement.Y, -1.5,1.2);
-            //GetNode<Node3D>("Pivot").RotateObjectLocal((new Vector3(1, 0, 0)), (mouseMovement.Y * 0.05f));
-            //GetNode<Node3D>("Pivot").RotateObjectLocal((new Vector3(0, 1, 0)), (mouseMovement.X * -0.05f));
-            //GetNode<Node3D>("Pivot").RotateY(mouseMovement.X * 0.05f);
-            //GetNode<Node3D>("Pivot").RotateX(mouseMovement.Y * -0.05f);
-            GetNode<Node3D>("Pivot").Rotation = new Vector3(mouseMovement.Y * 0.05f + GetNode<Node3D>("Pivot").Rotation.X, mouseMovement.X * -0.05f + GetNode<Node3D>("Pivot").Rotation.Y, GetNode<Node3D>("Pivot").Rotation.Z);
-            //GetNode<Node3D>("Pivot").Rotation.X.Equals(mouseMovement.Y * -0.05f + GetNode<Node3D>("Pivot").Rotation.X);
-
-            //if (GetNode<Node3D>("Pivot").Rotation.X > 50) GetNode<Node3D>("Pivot").RotateObjectLocal((new Vector3(1, 0, 0)), -GetNode<Node3D>("Pivot").Rotation.X);
-            //if (GetNode<Node3D>("Pivot").Rotation.X < -90) GetNode<Node3D>("Pivot").RotateObjectLocal((new Vector3(1, 0, 0)), GetNode<Node3D>("Pivot").Rotation.X);
+            pivot.Rotation = new Vector3(mouseMovement.Y * 0.01f + pivot.Rotation.X, mouseMovement.X * -0.01f + pivot.Rotation.Y, pivot.Rotation.Z);
         }
     }
 }
